766a3d0b45b6d2048c8ab8c3ebc97e7df8dd696f
ChenAiping <2761249451@qq.com>
2019-02-03 21:52:33 +0800

    kuaileyidian

---
+++ src/components/Dish.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="klyd-dish-info">
+    <div class="klyd-dish-content">
+      <div class="klyd-dish-list" @click="makeQrCode">
+        <img :src="imagesUrl" alt=""/>
+        <h4>{{data.title}}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      imagesUrl:""
+    }
+  },
+  props:['data'],
+  methods:{
+    makeQrCode(){
+      var url=this.$store.state.globalSettings.apiUrl+'/admin/dish/'+this.data.imgUrl;
+      // var url=url.ToBase64String();
+      // console.log(url);
+      var url=getBase64Image(url);
+      console.log(url);
+      this.imagesUrl=url;
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .klyd-dish-info{
+    padding:15px;
+    .klyd-dish-content{
+      display:block;
+    }
+  }
+</style>
+
--- src/views/dishList.vue
+++ src/views/dishList.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="klyd-dish-list">
-    <h1>菜品列表</h1>
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>菜品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>类品列表</el-breadcrumb-item>
+    </el-breadcrumb>
 
+    <br>
     <el-tabs type="border-card">
-        <el-tab-pane v-for="(c,i) in dishLish" :key="i" :label="c.cname">
+        <el-tab-pane v-for="(c,i) in dishList" :key="i">
           <span slot="label">
             <el-badge :value="c.dishLish.length">{{c.cname}}</el-badge>
           </span>
-
+          
           <el-row>
             <el-col v-for="(d,j) in c.dishLish" :key="j" :xs="12" :md="6" :lg="4" :xl="3">
-              <!-- <klyd-dish :data="d"></klyd-dish> -->
-              {{d.title}}
-              <img :src="require('../assets/img/dish/'+d.imgUrl)" style="max-width:100% "/>
+              <!-- 引入组件 -->
+              <klyd-dish :data="d"></klyd-dish>
+              <!-- {{d.title}}
+              <img :src="require('../assets/img/dish/'+d.imgUrl)" style="max-width:100%"> -->
             </el-col>
           </el-row>
         </el-tab-pane>
@@ -20,6 +26,7 @@
   </div>
 </template>
 <script>
+import Dish from '../components/Dish'
 export default {
   data(){
     return{
@@ -30,10 +37,21 @@ export default {
     // 异步获取菜品列表
     var url=this.$store.state.globalSettings.apiUrl+'/admin/dish';
     this.$axios.get(url).then(({data})=>{
+      console.log(data);
       this.dishList=data;
     }).catch((err)=>{
       console.log(err);
     })
+  },
+  components:{
+    'klyd-dish':Dish
   }
 }
 </script>
+<style lang="scss">
+  .el-tabs--border-card{
+    .el-tabs__nav-scroll{
+      padding-top:10px;
+    }
+  }
+</style>
